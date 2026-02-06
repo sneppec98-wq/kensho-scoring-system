abaa212de372472d7ffb2611f23a7f944addf2d9
kenshotechno-prog <kenshotechno@gmail.com>
2026-02-06 23:37:31 +0700

    chore: retrigger build v3.3.1 with timestamp update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Kensho Tech Manager - v3.3.1 (Build Trigger: 2026-02-05 19:30)
+// Kensho Tech Manager - v3.3.1 (Build Trigger: 2026-02-06 23:38)
 const { app, BrowserWindow, shell, ipcMain, dialog } = require('electron');
 const path = require('path');
 const { exec } = require('child_process');
